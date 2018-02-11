0e6444c5b15729c3bfe73c0c6980b0cd1baebe5b
psk2 <sai.krishna@isteer.in>
2018-02-11 21:33:54 +0530

    make it better

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-    module.exports = { mongoURI: "mongodb://<psk>:<root>@ds231588.mlab.com:31588/vidjot-prod" }
+    module.exports = { mongoURI: "mongodb://psk:root@ds231588.mlab.com:31588/vidjot-prod" }
 } else {
     module.exports = { mongoURI: "mongodb://localhost/vidjot-dev" }
 }
